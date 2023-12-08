d423c89b388ca89a1883b7fa6351c769f78022d2
JorgeAlfonsoLDiaz <1722110483@utectulancingo.edu.mx>
2023-12-07 20:09:10 -0600

    Se han implementado algunos detalles

--- main.py
+++ main.py
@@ -33,12 +33,20 @@ class Contacto(BaseModel):
 async def obtener_contactos():
     """Obtiene todos los contactos."""
     # DONE Consulta todos los contactos de la base de datos y los envia en un JSON
-    c = conn.cursor()
-    c.execute('SELECT * FROM contactos')
-    response = []
-    for row in c:
-        contacto = {"email":row[0], "nombre":row[1], "telefono":row[2]}
-        response.append(contacto)
+    try:
+        c = conn.cursor()
+        c.execute('SELECT * FROM contactos')
+        response = []
+        for row in c:
+            contacto = {"email":row[0], "nombre":row[1], "telefono":row[2]}
+            response.append(contacto)
+    
+    except:
+        response = {"mensaje":"Error al consultar los datos"}
+
+    finally:
+        c.close()
+    
     return response
 
 @app.post("/contactos")
@@ -82,5 +90,4 @@ async def eliminar_contacto(email: str):
     c.execute('DELETE FROM contactos WHERE email = ?', (email,))
     conn.commit()
     return {"message": "Contacto eliminado con éxito."}
-    
     
