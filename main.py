3aa278d4f7fda6439334cf92b563ae713acc2370
JorgeAlfonsoLDiaz <1722110483@utectulancingo.edu.mx>
2023-12-07 22:31:09 -0600

    Se implementó un sistema de manejo de errores más sencillo

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, HTTPException
+import fastapi
+import sqlite3
+
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-
-import sqlite3
 
 # Crea la base de datos
 conn = sqlite3.connect("contactos.db")
 
-app = FastAPI()
+app = fastapi.FastAPI()
 
 origins = [
     "https://contactos-api-frontend-heroku-f3a3001ed46a.herokuapp.com",
@@ -23,25 +23,26 @@ app.add_middleware(
 )
 
 class Contacto(BaseModel):
-    email: str
-    nombre: str
-    telefono: str
-
-class ErrorRespuesta(BaseModel):
-    mensaje: str
+    email : str
+    nombre : str
+    telefono : str
 
 # Rutas para las operaciones CRUD
 
 @app.get("/", status_code=200, description="Endpoint raíz de la API.", summary="Endpoint raíz.")
 async def obtener_contactos():
     """Obtiene todos los contactos."""
+    # DONE Consulta todos los contactos de la base de datos y los envia en un JSON
     try:
         c = conn.cursor()
         c.execute('SELECT * FROM contactos')
-        response = [{"email": row[0], "nombre": row[1], "telefono": row[2]} for row in c]
+        response = []
+        for row in c:
+            contacto = {"email":row[0], "nombre":row[1], "telefono":row[2]}
+            response.append(contacto) 
     
-    except Exception as e:
-        raise HTTPException(status_code=500, detail={"mensaje":"Error al consultar los datos"})
+    except:
+        response = {"mensaje":"Error al consultar los datos"}
 
     finally:
         c.close()
@@ -51,51 +52,42 @@ async def obtener_contactos():
 @app.post("/contactos", status_code=201, description="Endpoint para enviar datos a la API.", summary="Endpoint para enviar datos.")
 async def crear_contacto(contacto: Contacto):
     """Crea un nuevo contacto."""
-    try:
-        c = conn.cursor()
-        c.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
-                  (contacto.email, contacto.nombre, contacto.telefono))
-        conn.commit()
-    except Exception as e:
-        raise HTTPException(status_code=500, {"mensaje":"Error al crear el contacto"})
-
+    # DONE Inserta el contacto en la base de datos y responde con un mensaje
+    c = conn.cursor()
+    c.execute('INSERT INTO contactos (email, nombre, telefono) VALUES (?, ?, ?)',
+              (contacto.email, contacto.nombre, contacto.telefono))
+    conn.commit()
     return contacto
+
 
 @app.get("/contactos", status_code=200, description="Endpoint para consultar datos de la API.", summary="Endpoint para consulta.")
 async def obtener_contacto(email: str):
     """Obtiene un contacto por su email."""
+    # DONE Consulta el contacto por su email
     c = conn.cursor()
     c.execute('SELECT * FROM contactos WHERE email = ?', (email,))
     contacto = None
     for row in c:
-        contacto = {"email": row[0], "nombre": row[1], "telefono": row[2]}
-    
-    if not contacto:
-        raise HTTPException(status_code=404, detail={"mensaje":"Error al consulta el registro"})
-
+        contacto = {"email":row[0], "nombre":row[1], "telefono":row[2]}
     return contacto
 
 @app.put("/contactos/{email}", status_code=200, description="Endpoint para actualizar datos de la API.", summary="Endpoint para actualizar.")
 async def actualizar_contacto(email: str, contacto: Contacto):
     """Actualiza un contacto."""
-    try:
-        c = conn.cursor()
-        c.execute('UPDATE contactos SET nombre = ?, telefono = ?, email = ? WHERE email = ?',
-                  (contacto.nombre, contacto.telefono, contacto.email, email))
-        conn.commit()
-    except Exception as e:
-        raise HTTPException(status_code=500, detail={"mensaje":"Error al actualizar el registro."})
-
+    # DONE Actualiza el contacto en la base de datos
+    c = conn.cursor()
+    c.execute('UPDATE contactos SET nombre = ?, telefono = ?, email = ? WHERE email = ?',
+              (contacto.nombre, contacto.telefono, contacto.email, email))
+    conn.commit()
     return contacto
+
 
 @app.delete("/contactos/{email}", status_code=200, description="Endpoint para eliminar datos de la API.", summary="Endpoint para eliminar.")
 async def eliminar_contacto(email: str):
     """Elimina un contacto."""
-    try:
-        c = conn.cursor()
-        c.execute('DELETE FROM contactos WHERE email = ?', (email,))
-        conn.commit()
-    except Exception as e:
-        raise HTTPException(status_code=500, detail={"mensaje":"Error al consultar los datos"})
-
-    return {"mensaje": f"Contacto con email {email} eliminado con éxito."}
+    # DONE Elimina el contacto de la base de datos
+    c = conn.cursor()
+    c.execute('DELETE FROM contactos WHERE email = ?', (email,))
+    conn.commit()
+    return {"message": "Contacto eliminado con éxito."}
+    
